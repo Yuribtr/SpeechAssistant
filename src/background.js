3c769c8e78c7537785746a227ff1697f195f75ad
Yuri <yuribtr@users.noreply.github.com>
2021-10-28 22:59:59 +0300

    Refactor content and background scripts, remove vmeter script, update version to 1.0

--- src/background.js
+++ src/background.js
@@ -3,20 +3,25 @@ let isDebug = false;
 // Listen for messages from the content
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
     consoleLog("Background script received message from: " + msg.from + ", with subject: " + msg.subject);
-    // First, validate the message's structure.
-    if ((msg.from === 'content') && (msg.subject === 'recognizeStarted')) {
-        toggleButton('icon_19_on.png');
+    // First, validate the message"s structure.
+    if ((msg.from === "content") && (msg.subject === "recognizeStarted")) {
+        toggleButton("icon_19_on.png");
     }
-    if ((msg.from === 'content') && (msg.subject === 'recognizeStopped')) {
-        toggleButton('icon_19_off.png');
+    if ((msg.from === "content") && (msg.subject === "recognizeStopped")) {
+        toggleButton("icon_19_off.png");
+    }
+    if ((msg.from === "content") && (msg.subject === "enableDebug")) {
+        isDebug = true;
     }
 });
 
 function toggleButton(iconPath) {
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        chrome.browserAction.setIcon({tabId: tabs[0].id, path: iconPath});
-        consoleLog('Icon changed to ' + iconPath);
-    });
+    chrome.tabs.query(
+        {"active": true, "lastFocusedWindow": true},
+        function (tabs) {
+            chrome.browserAction.setIcon({tabId: tabs[0].id, path: iconPath});
+            consoleLog("Icon changed to " + iconPath);
+        });
 }
 
 function timeStamp() {
@@ -26,6 +31,6 @@ function timeStamp() {
 
 function consoleLog(text) {
     if (isDebug) {
-        console.log(timeStamp() + ' ' + text);
+        console.log(timeStamp() + " | " + text + " [background.js]");
     }
 }
--- src/content.js
+++ src/content.js
@@ -1,5 +1,4 @@
-let context = null;
-let vMeterWorkletNode = null;
+let lastEventTime = Date.now();
 let inputs, index, activeInput, buffer;
 let isDebug = false;
 let isStopped = true;
@@ -38,65 +37,34 @@ let errorCodes = {
 };
 let backColor = "DarkOrchid";
 let backErrorColor = "Crimson";
-let lastEventTime = 0;
-let currEventTime = 0;
 let isEnabled = 0;
-//detecting current domain name
+//detect current domain name
 let host = window.location.host;
-consoleLog("Extension loaded at " + host);
+consoleLog("Extension loaded at " + host, true);
 
-//reading settings
+//read settings
 chrome.storage.sync.get([
     "favoriteColor",
     "recognitionLang",
     "isDebug",
     host
-], function (items) {
-    if (typeof items.isDebug === "undefined") {
-        items.isDebug = false;
-    }
-    isDebug = items.isDebug;
-
-    if (typeof items.favoriteColor === "undefined") {
-        items.favoriteColor = "DarkOrchid";
-    }
-    backColor = items.favoriteColor;
-    //if lang not specified, assume this will be default browser language
-    if (typeof items.recognitionLang === "undefined") {
-        consoleLog("Storage: not found any recognitionLang");
-        //check, if default browser language is in list of supported SpeechRecognition languages
-        let tmpLang = window.navigator.language || window.navigator.userLanguage;
-        if (checkEncodingSupport(tmpLang)) {
-            consoleLog("Storage: browser language " + tmpLang + " is supported!");
-            items.recognitionLang = tmpLang;
-        } else {
-            //if not, fallback to english GB lang
-            consoleLog("Storage: browser language " + tmpLang + " is not supported! Falling back to 'en-GB'");
-            items.recognitionLang = "en-GB";
-        }
-    }
-    recognitionLang = items.recognitionLang;
-    isEnabled = items[host] === 1 ? 1 : 0;
-    consoleLog("Storage: set recognitionLang: " + recognitionLang);
-    consoleLog("Storage: set favoriteColor: " + backColor);
-    consoleLog("Storage: set isEnabled: " + isEnabled);
-    //because this callback is asynchronous, we have to start recognizing upon receipt of settings
-    if (isEnabled) start();
-});
+], initSettings);
 
 // Listen for messages from the popup.
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
+chrome.runtime.onMessage.addListener(onPopupAction);
+
+function onPopupAction(msg, sender, response) {
     consoleLog("Received message from: " + msg.from + ", with subject: " + msg.subject);
     // First, validate the message"s structure.
     if ((msg.from === "popup") && (msg.subject === "startRecognize")) {
         isEnabled = true;
-        start();
+        doStartRecognize();
     }
     if ((msg.from === "popup") && (msg.subject === "stopRecognize")) {
         isEnabled = false;
-        stop();
+        doStopRecognize();
     }
-});
+}
 
 function checkEncodingSupport(encoding) {
     let result = false;
@@ -128,7 +96,161 @@ function sendMessageToBackground(text) {
     });
 }
 
-function start() {
+function initSettings(items) {
+    if (typeof items.isDebug === "undefined") {
+        items.isDebug = false;
+    }
+    isDebug = items.isDebug;
+    if (isDebug) {
+        sendMessageToBackground("enableDebug");
+    }
+    if (typeof items.favoriteColor === "undefined") {
+        items.favoriteColor = "DarkOrchid";
+    }
+    backColor = items.favoriteColor;
+    //if lang not specified, assume this will be default browser language
+    if (typeof items.recognitionLang === "undefined") {
+        consoleLog("Storage: not found any recognitionLang");
+        //check, if default browser language is in list of supported SpeechRecognition languages
+        let tmpLang = window.navigator.language || window.navigator.userLanguage;
+        if (checkEncodingSupport(tmpLang)) {
+            consoleLog("Storage: browser language " + tmpLang + " is supported!");
+            items.recognitionLang = tmpLang;
+        } else {
+            //if not, fallback to english GB lang
+            consoleLog("Storage: browser language " + tmpLang + " is not supported! Falling back to 'en-GB'");
+            items.recognitionLang = "en-GB";
+        }
+    }
+    recognitionLang = items.recognitionLang;
+    isEnabled = items[host] === 1 ? 1 : 0;
+    consoleLog("Storage: set recognitionLang: " + recognitionLang);
+    consoleLog("Storage: set favoriteColor: " + backColor);
+    consoleLog("Storage: set isEnabled: " + isEnabled);
+    //because this callback is asynchronous, we have to start recognizing upon receipt of settings
+    if (isEnabled) doStartRecognize();
+}
+
+function onRecognitionResult(event) {
+    consoleLog("onresult fired");
+    //divide into words detected results & get the latest
+    //total results detected
+    let resultsLength = event.results.length - 1;
+    //get length of latest results
+    let ArrayLength = event.results[resultsLength].length - 1;
+    //get last word detected
+    let saidWord = event.results[resultsLength][ArrayLength].transcript;
+    //get rounded coefficient of reliability or accuracy (1 is max)
+    let resultConfidence = Math.round(event.results[resultsLength][ArrayLength].confidence * 100) / 100;
+    if (activeInput !== null) {
+        buffer = buffer + " " + saidWord;
+        if (isDebug)
+            buffer = buffer + " [" + resultConfidence + "]";
+        activeInput.value = activeInput.value + buffer;
+        buffer = "";
+        if (resultConfidence < recognitionConfLimit) {
+            consoleLog("Low quality detection!" + " [" + resultConfidence + "]");
+            showConfidenceWarning(true);
+        } else {
+            showConfidenceWarning(false);
+        }
+    }
+}
+
+function onRecognitionStart() {
+    isStopped = false;
+    lastError = null;
+    informerPopupMessage.innerText = messageInAir;
+    informerPopupText.style.background = backColor;
+    informerPopupConfidence.style.background = backErrorColor;
+    isStopRequested = isStartRequested = false;
+    consoleLog("Speech recognition service has started");
+}
+
+function onRecognitionError(event) {
+    lastError = event.error;
+    let myMessage = event.message === null ? event.message : errorCodes[event.error];
+    //if new error codes without messages will be added in future in browser, at least we will show those error codes
+    informerPopupMessage.innerText = typeof myMessage === "undefined" ? event.error : myMessage;
+    consoleLog("Error: " + myMessage + " (" + event.error + ")");
+}
+
+function onRecognitionEnd() {
+    consoleLog("Speech recognition service disconnected");
+    isStopped = true;
+    isStopRequested = false;
+    //restart if we move from another input, or if service disconnected due to silence
+    if (((isStartRequested) || (activeInput !== null)) && ((lastError === null) || (lastError === "no-speech"))) {
+        buffer = "";
+        recognition.start();
+    }
+    if ((lastError !== null) && (lastError !== "no-speech")) {
+        informerPopupText.style.background = backErrorColor;
+    }
+}
+
+function onSpeechEnd() {
+    consoleLog("Speech has stopped being detected");
+}
+
+function onSoundEnd(event) {
+    consoleLog("Sound has stopped being received");
+}
+
+function onInputFocus() {
+    consoleLog("onFocus fired " + this);
+    //move input focus to selected component
+    activeInput = this;
+    //check if there enough space above focused element to show our popup
+    consoleLog("Focused element at " + getOffset(this).top);
+    if (getOffset(this).top > 100) {
+        insertBefore(informerPopup, this);
+        informerPopupText.classList.remove("bottom");
+    } else {
+        insertAfter(informerPopup, this);
+
+        informerPopupText.classList.add("bottom");
+        //consoleLog("Setting offset from focused element to "+this.offsetHeight+" + 10px");
+        informerPopupText.style.top = this.offsetHeight + 5 + "px";
+        informerPopupText.style.left = "-" + this.offsetWidth + "px";
+    }
+    informerPopupText.classList.add("show");
+    //normal condition
+    if (!isStartRequested && isStopped) {
+        //isStopped = false;
+        isStartRequested = true;
+        isStopRequested = false;
+        buffer = "";
+        recognition.start();
+    }
+    //if we moves from another input, let"s schedule new start
+    if (isStopRequested && !isStopped) {
+        isStartRequested = true;
+    }
+}
+
+function onInputBlur() {
+    consoleLog("onBlur fired " + this);
+    informerPopupText.classList.remove("show");
+    //do stop, if input focus was NOT moved to other component, otherwise ignore this event
+    if (activeInput === this) {
+        isStartRequested = false;
+        isStopRequested = true;
+        activeInput = null;
+        recognition.stop();
+    }
+}
+
+function bindEventsToInputs() {
+    //Extension loaded after DOM ready, so we can iterate all inputs
+    inputs = document.querySelectorAll("input[type=text], input[type=password], input[type=email], input[type=search], input[type=url], input[type=tel], textarea");
+    for (index = 0; index < inputs.length; ++index) {
+        inputs[index].onfocus = onInputFocus;
+        inputs[index].onblur = onInputBlur;
+    }
+}
+
+function doStartRecognize() {
     consoleLog("Monitoring started...");
     //changing icon at Chrome panel
     sendMessageToBackground("recognizeStarted");
@@ -141,124 +263,14 @@ function start() {
     recognition.interimResults = false;
     //receive only one variant of recognition
     recognition.maxAlternatives = 1;
-    recognition.onresult = function (event) {
-        consoleLog("onresult fired");
-        //divide into words detected results & get the latest
-        //total results detected
-        let resultsLength = event.results.length - 1;
-        //get length of latest results
-        let ArrayLength = event.results[resultsLength].length - 1;
-        //get last word detected
-        let saidWord = event.results[resultsLength][ArrayLength].transcript;
-        //get rounded coefficient of reliability or accuracy (1 is max)
-        let resultConfidence = Math.round(event.results[resultsLength][ArrayLength].confidence * 100) / 100;
-        if (window.activeInput !== null) {
-            buffer = buffer + " " + saidWord;
-            if (isDebug)
-                buffer = buffer + " [" + resultConfidence + "]";
-            window.activeInput.value = window.activeInput.value + buffer;
-            buffer = "";
-            if (resultConfidence < recognitionConfLimit) {
-                consoleLog("Low quality detection!" + " [" + resultConfidence + "]");
-                showConfidenceWarning(true);
-            } else {
-                showConfidenceWarning(false);
-            }
-        }
-    };
 
-    recognition.onstart = function () {
-        isStopped = false;
-        lastError = null;
-        informerPopupMessage.innerText = messageInAir;
-        informerPopupText.style.background = backColor;
-        informerPopupConfidence.style.background = backErrorColor;
-        isStopRequested = isStartRequested = false;
-        startVMeter().then(r => consoleLog("Speech recognition service has started"));
-        consoleLog("Speech recognition service has started");
-    };
-
-    recognition.onerror = function (event) {
-        lastError = event.error;
-        let myMessage = event.message === null ? event.message : errorCodes[event.error];
-        //if new error codes without messages will be added in future in browser, at least we will show those error codes
-        informerPopupMessage.innerText = typeof myMessage === "undefined" ? event.error : myMessage;
-        consoleLog("Error: " + myMessage + " (" + event.error + ")");
-    };
-
-    recognition.onend = function () {
-        consoleLog("Speech recognition service disconnected");
-        isStopped = true;
-        isStopRequested = false;
-        //restart if we move from another input, or if service disconnected due to silence
-        if (((isStartRequested) || (window.activeInput !== null)) && ((lastError === null) || (lastError === "no-speech"))) {
-            buffer = "";
-            context.resume();
-            recognition.start();
-        }
-        if ((lastError !== null) && (lastError !== "no-speech")) {
-            informerPopupText.style.background = backErrorColor;
-        }
-        stopVMeter()
-    };
-
-    recognition.onspeechend = function () {
-        consoleLog("Speech has stopped being detected");
-    };
-
-    recognition.onsoundend = function (event) {
-        consoleLog("Sound has stopped being received");
-    };
-
-    //Extension loaded after DOM ready, so we can iterate all inputs
-    inputs = document.querySelectorAll("input[type=text], input[type=password], input[type=email], input[type=search], input[type=url], input[type=tel], textarea");
-    for (index = 0; index < inputs.length; ++index) {
-        inputs[index].onfocus = function () {
-            consoleLog("onFocus fired " + this);
-            //move input focus to selected component
-            window.activeInput = this;
-            //check if there enough space above focused element to show our popup
-            consoleLog("Focused element at " + getOffset(this).top);
-            if (getOffset(this).top > 100) {
-                insertBefore(informerPopup, this);
-                informerPopupText.classList.remove("bottom");
-            } else {
-                insertAfter(informerPopup, this);
-
-                informerPopupText.classList.add("bottom");
-                //consoleLog("Setting offset from focused element to "+this.offsetHeight+" + 10px");
-                informerPopupText.style.top = this.offsetHeight + 5 + "px";
-                informerPopupText.style.left = "-" + this.offsetWidth + "px";
-            }
-            informerPopupText.classList.add("show");
-            //normal condition
-            if (!isStartRequested && isStopped) {
-                //isStopped = false;
-                isStartRequested = true;
-                isStopRequested = false;
-                buffer = "";
-                //context.resume();
-                // startVMeter();
-                recognition.start();
-            }
-            //if we moves from another input, let"s schedule new start
-            if (isStopRequested && !isStopped) {
-                isStartRequested = true;
-            }
-        };
-        inputs[index].onblur = function () {
-            consoleLog("onBlur fired " + this);
-            informerPopupText.classList.remove("show");
-            //do stop, if input focus was NOT moved to other component, otherwise ignore this event
-            if (window.activeInput === this) {
-                isStartRequested = false;
-                isStopRequested = true;
-                window.activeInput = null;
-                // stopVMeter();
-                recognition.stop();
-            }
-        }
-    }
+    recognition.onresult = onRecognitionResult;
+    recognition.onstart = onRecognitionStart;
+    recognition.onerror = onRecognitionError;
+    recognition.onend = onRecognitionEnd;
+    recognition.onspeechend = onSpeechEnd;
+    recognition.onsoundend = onSoundEnd;
+    bindEventsToInputs();
 }
 
 function getOffset(el) {
@@ -269,9 +281,9 @@ function getOffset(el) {
     };
 }
 
-function stop() {
-    if ((typeof window.activeInput !== "undefined") && (window.activeInput !== null)) {
-        window.activeInput.blur();
+function doStopRecognize() {
+    if ((typeof activeInput !== "undefined") && (activeInput !== null)) {
+        activeInput.blur();
     }
     //Extension loaded after DOM ready, so we can iterate all inputs
     inputs = document.querySelectorAll("input[type=text], input[type=password], input[type=email], input[type=search], input[type=url], input[type=tel], textarea");
@@ -308,56 +320,15 @@ function timeStamp() {
     return d.toLocaleTimeString();
 }
 
-function consoleLog(text) {
-    if (isDebug) {
-        currEventTime = Date.now();
-        let flags = " [" + (currEventTime - lastEventTime) + "ms";
-        if (isStopped) flags = flags + " isStopped";
-        if (isStopRequested) flags = flags + " isStopRequested";
-        if (isStartRequested) flags = flags + " isStartRequested";
-        flags = flags + "]";
-        console.log(timeStamp() + " " + text + flags);
+function consoleLog(text, force_log = false) {
+    if (isDebug || force_log) {
+        let currEventTime = Date.now();
+        let flags = " [" + (currEventTime - lastEventTime) + "ms,";
+        if (isStopped) flags = flags + " isStopped,";
+        if (isStopRequested) flags = flags + " isStopRequested,";
+        if (isStartRequested) flags = flags + " isStartRequested,";
+        flags = flags + " content.js]";
+        console.log(timeStamp() + " | " + text + flags);
         lastEventTime = currEventTime;
-    }
-}
-
-//AudioWorkletNode needed for creation and async communication with AudioWorkletProcessor
-class VMeterWorkletNode extends AudioWorkletNode {
-    constructor(context) {
-        super(context, "vmonitor-processor");
-        this.port.onmessage = this.handleMessage.bind(this);
-    }
-
-    handleMessage(event) {
-        consoleLog("AudioWorkletNode received message: " + event.data.message);
-    }
-
-    doStop() {
-        this.port.postMessage({
-            message: "stopMonitoring"
-        });
-    }
-}
-
-async function startVMeter() {
-    consoleLog("VMeter starting");
-    const processorURL = chrome.runtime.getURL("vmeter.js");
-    if (!context) {
-        context = new AudioContext();
-    }
-    await context.audioWorklet.addModule(processorURL);
-    consoleLog("VMeter module added");
-    vMeterWorkletNode = new VMeterWorkletNode(context);
-    vMeterWorkletNode.connect(context.destination);
-    const streamFromUserMedia = await navigator.mediaDevices.getUserMedia({audio: true});
-    context.createMediaStreamSource(streamFromUserMedia).connect(vMeterWorkletNode);
-}
-
-async function stopVMeter() {
-    //TODO fix problem of volume meter during fast toggling between HTML inputs
-    consoleLog("VMeter stopping");
-    if (vMeterWorkletNode) {
-        await vMeterWorkletNode.doStop();
-        vMeterWorkletNode = null;
     }
 }
